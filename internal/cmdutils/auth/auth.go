f7cfe4e7543d380a01b7d56a271ae2770f6521bb
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2023-04-20 12:35:42 +0200

    fix(api): remove unused function `IsAuthenticated`

--- internal/cmdutils/auth/auth.go
+++ internal/cmdutils/auth/auth.go
@@ -1,63 +1,16 @@
 package auth
 
 import (
-	"errors"
-	"fmt"
-
-	"github.com/spf13/viper"
-
 	"code-intelligence.com/cifuzz/internal/api"
-	"code-intelligence.com/cifuzz/internal/cmdutils"
 	"code-intelligence.com/cifuzz/internal/cmdutils/login"
 	"code-intelligence.com/cifuzz/internal/tokenstorage"
-	"code-intelligence.com/cifuzz/pkg/cicheck"
 	"code-intelligence.com/cifuzz/pkg/dialog"
 	"code-intelligence.com/cifuzz/pkg/log"
 	"code-intelligence.com/cifuzz/pkg/messaging"
 )
 
-// IsAuthenticated checks if the user is authenticated with the server.
-func IsAuthenticated(server string, context messaging.MessagingContext) (bool, error) {
-	interactive := viper.GetBool("interactive")
-	if cicheck.IsCIEnvironment() {
-		interactive = false
-	}
-
-	server, err := api.ValidateAndNormalizeServerURL(server)
-	if err != nil {
-		return false, cmdutils.WrapSilentError(err)
-	}
-
-	authenticated, err := GetAuthStatus(server)
-	if err != nil {
-		var connErr *api.ConnectionError
-		if errors.As(err, &connErr) {
-			log.Warn("Connection to API failed. Skipping sync.")
-			log.Debugf("Connection error: %s (continuing gracefully)", connErr)
-			return false, nil
-		} else {
-			fmt.Println("AUTH STATUS CHECK ERROR")
-			return false, cmdutils.WrapSilentError(err)
-		}
-	}
-
-	if interactive && !authenticated {
-		// establish server connection to check user auth
-		authenticated, err = ShowServerConnectionDialog(server, context)
-		if err != nil {
-			var connErr *api.ConnectionError
-			if errors.As(err, &connErr) {
-				log.Warn("Connection to API failed. Skipping sync.")
-				log.Debugf("Connection error: %v (continuing gracefully)", connErr)
-				return false, nil
-			} else {
-				return false, cmdutils.WrapSilentError(err)
-			}
-		}
-	}
-	return authenticated, nil
-}
-
+// GetAuthStatus returns the authentication status of the user
+// for the given server
 func GetAuthStatus(server string) (bool, error) {
 	// Obtain the API access token
 	token := login.GetToken(server)
