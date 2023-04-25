e9d0d7e99c33fe6a2723677853821ac0cf39d60e
Florian Gläßer <florian.glaesser@code-intelligence.com>
2023-04-25 15:08:25 +0200

    feat(integration-tests): add spring boot maven example

---
+++ integration-tests/java-maven-spring/java_maven_spring_test.go
@@ -0,0 +1,48 @@
+package spring
+
+import (
+	"path/filepath"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"code-intelligence.com/cifuzz/integration-tests/shared"
+	builderPkg "code-intelligence.com/cifuzz/internal/builder"
+	"code-intelligence.com/cifuzz/internal/testutil"
+	"code-intelligence.com/cifuzz/util/fileutil"
+)
+
+func TestIntegration_MavenSpring(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	testutil.RegisterTestDepOnCIFuzz()
+	installDir := shared.InstallCIFuzzInTemp(t)
+	t.Cleanup(func() { fileutil.Cleanup(installDir) })
+	cifuzz := builderPkg.CIFuzzExecutablePath(filepath.Join(installDir, "bin"))
+
+	// Copy testdata
+	projectDir := shared.CopyTestdataDir(t, "spring-maven")
+	defer fileutil.Cleanup(projectDir)
+
+	cifuzzRunner := shared.CIFuzzRunner{
+		CIFuzzPath:      cifuzz,
+		DefaultWorkDir:  projectDir,
+		DefaultFuzzTest: "com.example.GreeterApplicationTests",
+	}
+
+	// Run the fuzz test
+	expectedOutputsExp := []*regexp.Regexp{
+		regexp.MustCompile(`High: SQL Injection`),
+	}
+	cifuzzRunner.Run(t, &shared.RunOptions{
+		ExpectedOutputs: expectedOutputsExp,
+	})
+
+	// Check that the findings command lists the finding
+	findings := shared.GetFindings(t, cifuzz, projectDir)
+	assert.Len(t, findings, 1)
+	assert.Contains(t, findings[0].Details, "SQL Injection")
+}
