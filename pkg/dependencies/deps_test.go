880197ed84ba7995fd6b28fcf8cada963ac44d54
mlsandner <maren.sandner@code-intelligence.com>
2023-05-09 11:30:39 +0200

    test(deps): special case gradle in dependencies

--- pkg/dependencies/deps_test.go
+++ pkg/dependencies/deps_test.go
@@ -2,6 +2,7 @@ package dependencies
 
 import (
 	"errors"
+	"path/filepath"
 	"testing"
 
 	"github.com/Masterminds/semver"
@@ -85,5 +86,32 @@ func TestCheck_UnableToGetVersion(t *testing.T) {
 	finder.On("CMakePath").Return("cmake", nil)
 
 	err := check(keys, deps, finder, "")
+	require.NoError(t, err)
+}
+
+// TestCheck_SpecialCaseGradle tests if the special case of gradle is handled
+// correctly. The preferred way of using gradle is using the 'gradlew' (wrapper)
+// in the project dir and this should also work without needing gradle to be
+// installed at all. If there is no gradlew or an empty project dir is given
+// it should default back to needing gradle installed.
+func TestCheck_SpecialCaseGradle(t *testing.T) {
+	keys := []Key{Gradle}
+	deps := getDeps(keys)
+
+	// no gradle and no gradlew
+	finder := &mocks.RunfilesFinderMock{}
+	finder.On("GradlePath").Return("", errors.New("missing-error"))
+	err := check(keys, deps, finder, "")
+	require.Error(t, err)
+
+	// gradle but no gradlew
+	finder = &mocks.RunfilesFinderMock{}
+	finder.On("GradlePath").Return("gradle", nil)
+	err = check(keys, deps, finder, "")
+	require.NoError(t, err)
+
+	// no gradle but gradlew in project dir
+	finder = &mocks.RunfilesFinderMock{}
+	err = check(keys, deps, finder, filepath.Join("testdata", "gradle"))
 	require.NoError(t, err)
 }
