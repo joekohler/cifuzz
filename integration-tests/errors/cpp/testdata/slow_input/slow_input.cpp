be6ecf5c551d28351e002203a5c00e020494e31c
Florian Gläßer <florian.glaesser@code-intelligence.com>
2023-06-01 17:27:14 +0200

    fix(integration-tests): fix slow input cpp test on Windows

--- integration-tests/errors/cpp/testdata/slow_input/slow_input.cpp
+++ integration-tests/errors/cpp/testdata/slow_input/slow_input.cpp
@@ -1,9 +1,22 @@
-#include <string>
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <unistd.h>
+#endif
+
 using namespace std;
 
-void slow(string c) {
-    if (c == "FUZZING") {
-        sleep(10);
+void slow(size_t size) {
+    // Don't show the undefined behavior if the input is empty, so that
+    // we can test that the fuzzer correctly emits the test input that
+    // caused the undefined behavior.
+    if (size == 0) {
+        return;
     }
+
+    #ifdef _WIN32
+        Sleep(2000);
+    #else
+        sleep(10);
+    #endif
 }
