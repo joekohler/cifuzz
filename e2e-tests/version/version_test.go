1b25f571478535d7d97f518e33e2bf0c91d634af
Adrian Dombeck <adrian.dombeck@code-intelligence.com>
2023-06-26 19:00:58 +0200

    test(e2e): Add test for --version option

---
+++ e2e-tests/version/version_test.go
@@ -0,0 +1,27 @@
+package e2e
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"code-intelligence.com/cifuzz/e2e-tests"
+)
+
+var versionTests = &[]e2e.TestCase{
+	{
+		Description: "using --version prints the version + os/arch",
+		Command:     "",
+		Args:        []string{"--version"},
+		Assert: func(t *testing.T, output e2e.CommandOutput) {
+			assert.EqualValues(t, 0, output.ExitCode)
+			assert.Equal(t, output.Stderr, "")
+			assert.Contains(t, output.Stdout, "cifuzz version ")
+			assert.Contains(t, output.Stdout, "Running on ")
+		},
+	},
+}
+
+func TestVersion(t *testing.T) {
+	e2e.RunTests(t, *versionTests)
+}
