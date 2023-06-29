d006851176ceaa9637f0ac105ad716727b142e47
Jochen <jochen.hilgers@code-intelligence.com>
2023-06-29 13:49:44 +0200

    fix: given version was not applied during installer build
    ... introduced by:
    https://github.com/CodeIntelligenceTesting/cifuzz/pull/859/commits/deb8311930cef11a67e66e5a1e8b8bf5da263a48

---
+++ internal/builder/builder_test.go
@@ -0,0 +1,39 @@
+package builder
+
+import (
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"code-intelligence.com/cifuzz/internal/testutil"
+)
+
+func TestIntegration_Version(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	version := "1.2.3-test"
+	testDir := testutil.MkdirTemp(t, "", "builder-test-*")
+	builder, err := NewCIFuzzBuilder(Options{
+		Version:   version,
+		TargetDir: testDir,
+		GOOS:      runtime.GOOS,
+		GOARCH:    runtime.GOARCH,
+	})
+	require.NoError(t, err)
+
+	err = builder.BuildCIFuzz()
+	require.NoError(t, err)
+
+	cifuzz := filepath.Join(testDir, "bin", "cifuzz")
+	out, err := exec.Command(cifuzz, "--version").Output()
+	require.NoError(t, err)
+
+	assert.Contains(t, string(out), version)
+
+}
