0cb7518db19f09beed8a64f35972cf33098a810a
Adrian Dombeck <adrian.dombeck@code-intelligence.com>
2023-07-26 14:16:15 +0200

    test(e2e): More readable error messages
    * Use `require` instead of `assert` to ensure that the error message is at
      the end of the log where it's easy to find
    * Don't print the full stdout/stderr again in the error message

--- e2e-tests/e2e-test-runner-assertions.go
+++ e2e-tests/e2e-test-runner-assertions.go
@@ -1,38 +1,49 @@
 package e2e
 
-import "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/stretchr/testify/require"
+)
 
 func (co *CommandOutput) Success() *CommandOutput {
-	assert.EqualValues(co.t, 0, co.ExitCode)
+	require.EqualValues(co.t, 0, co.ExitCode)
 	return co
 }
 
 func (co *CommandOutput) Failed() *CommandOutput {
-	assert.NotEqualValues(co.t, 0, co.ExitCode)
+	require.NotEqualValues(co.t, 0, co.ExitCode)
 	return co
 }
 
 func (co *CommandOutput) OutputContains(expected string) *CommandOutput {
-	assert.Contains(co.t, co.Stdout, expected)
+	if !strings.Contains(co.Stdout, expected) {
+		require.FailNow(co.t, fmt.Sprintf("stdout does not contain %q", expected))
+	}
 	return co
 }
 
 func (co *CommandOutput) OutputNotContains(expected string) *CommandOutput {
-	assert.NotContains(co.t, co.Stdout, expected)
+	if strings.Contains(co.Stdout, expected) {
+		require.FailNow(co.t, fmt.Sprintf("stdout contains %q", expected))
+	}
 	return co
 }
 
 func (co *CommandOutput) ErrorContains(expected string) *CommandOutput {
-	assert.Contains(co.t, co.Stderr, expected)
+	if !strings.Contains(co.Stderr, expected) {
+		require.FailNow(co.t, fmt.Sprintf("stderr does not contain %q", expected))
+	}
 	return co
 }
 
 func (co *CommandOutput) NoOutput() *CommandOutput {
-	assert.Empty(co.t, co.Stdout)
+	require.Empty(co.t, co.Stdout)
 	return co
 }
 
 func (co *CommandOutput) NoError() *CommandOutput {
-	assert.Empty(co.t, co.Stderr)
+	require.Empty(co.t, co.Stderr)
 	return co
 }
