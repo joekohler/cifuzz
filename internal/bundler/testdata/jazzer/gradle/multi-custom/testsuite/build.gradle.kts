b056a147944ea72e53028344ca5f0276989ccc92
Florian Gläßer <florian.glaesser@code-intelligence.com>
2023-08-14 11:28:56 +0200

    chore(deps): update cifuzz-gradle-plugin to v1.4.0

--- internal/bundler/testdata/jazzer/gradle/multi-custom/testsuite/build.gradle.kts
+++ internal/bundler/testdata/jazzer/gradle/multi-custom/testsuite/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("com.code-intelligence.cifuzz") version "1.3.0"
+    id("com.code-intelligence.cifuzz") version "1.4.0"
 }
 
 sourceSets.getByName("test") {
