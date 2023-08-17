6b5ddc824e1675c5de3555baf089f2a1ea0c11d8
Adrian Dombeck <adrian.dombeck@code-intelligence.com>
2023-08-17 15:08:50 +0200

    fix: Don't print JSON messages for seed corpus reports

--- internal/cmd/run/reporthandler/reporthandler.go
+++ internal/cmd/run/reporthandler/reporthandler.go
@@ -93,12 +93,17 @@ func NewReportHandler(fuzzTest string, options *ReportHandlerOptions) (*ReportHa
 func (h *ReportHandler) Handle(r *report.Report) error {
 	var err error
 
-	if r.SeedCorpus != "" {
-		h.ManagedSeedCorpusDir = r.SeedCorpus
-	}
-
-	if r.GeneratedCorpus != "" {
-		h.GeneratedCorpusDir = r.GeneratedCorpus
+	if r.SeedCorpus != "" || r.GeneratedCorpus != "" {
+		// This report was only sent to update the seed corpus directory path
+		// which we use later to count the number of seeds. We don't want to
+		// print the report, so we return early.
+		if r.SeedCorpus != "" {
+			h.ManagedSeedCorpusDir = r.SeedCorpus
+		}
+		if r.GeneratedCorpus != "" {
+			h.GeneratedCorpusDir = r.GeneratedCorpus
+		}
+		return nil
 	}
 
 	if r.Status == report.RunStatusInitializing && !h.initStarted {
