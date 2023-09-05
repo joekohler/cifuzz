08305d9b8c9890873dcc26b8634e8661f2dc482d
Jochen <jochen.hilgers@code-intelligence.com>
2023-09-05 14:36:06 +0200

    feat(updater): adjust updater tool to support jazzerjs
    ... and use arguments instead of fixed versions, for example
    go run ./tools/updater/updater.go --dependency jazzerjs --version 1.7.0

--- tools/updater/updater.go
+++ tools/updater/updater.go
@@ -2,37 +2,79 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"os/exec"
 	"regexp"
+
+	"github.com/Masterminds/semver"
+	"github.com/pkg/errors"
+	"github.com/spf13/pflag"
+
+	"code-intelligence.com/cifuzz/pkg/log"
 )
 
-const GradlePluginVersion = "1.2.0"
-
 func main() {
-	updateGradlePluginVersion("examples/gradle/build.gradle")
-	updateGradlePluginVersion("examples/gradle-kotlin/build.gradle.kts")
-	updateGradlePluginVersion("examples/gradle-multi/testsuite/build.gradle.kts")
-	updateGradlePluginVersion("pkg/messaging/instructions/gradle")
-	updateGradlePluginVersion("pkg/messaging/instructions/gradlekotlin")
-	updateGradlePluginVersion("internal/bundler/testdata/jazzer/gradle/multi-custom/testsuite/build.gradle.kts")
+
+	flags := pflag.NewFlagSet("updater", pflag.ExitOnError)
+	deps := flags.String("dependency", "", "which dependency to update eg. gradle-plugin, jazzerjs")
+	version := flags.String("version", "", "target version to update to, for example 1.2.3")
+
+	if err := flags.Parse(os.Args); err != nil {
+		log.Error(errors.WithStack(err))
+		os.Exit(1)
+	}
+
+	_, err := semver.NewVersion(*version)
+	if err != nil {
+		log.Error(errors.WithStack(err))
+		os.Exit(1)
+	}
+
+	switch *deps {
+	case "gradle-plugin":
+		updateGradlePluginVersion("examples/gradle/build.gradle", *version)
+		updateGradlePluginVersion("examples/gradle-kotlin/build.gradle.kts", *version)
+		updateGradlePluginVersion("examples/gradle-multi/testsuite/build.gradle.kts", *version)
+		updateGradlePluginVersion("pkg/messaging/instructions/gradle", *version)
+		updateGradlePluginVersion("pkg/messaging/instructions/gradlekotlin", *version)
+		updateGradlePluginVersion("internal/bundler/testdata/jazzer/gradle/multi-custom/testsuite/build.gradle.kts", *version)
+	case "jazzerjs":
+		updateJazzerNpm("examples/nodejs", *version)
+		updateJazzerNpm("examples/nodejs-typescript", *version)
+	default:
+		log.Error(errors.New("unsupported dependency selected"))
+		os.Exit(1)
+	}
 }
 
-func updateGradlePluginVersion(path string) {
+func updateJazzerNpm(path string, version string) {
+	cmd := exec.Command("npm", "install", "--save-dev", fmt.Sprintf("@jazzer.js/jest-runner@%s", version))
+	cmd.Dir = path
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+}
+
+func updateGradlePluginVersion(path string, version string) {
 	content, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err)
+		os.Exit(1)
 	}
 	buildFile := string(content)
 
 	re := regexp.MustCompile(`("com.code-intelligence.cifuzz"\)? version ")(?P<version>\d+.\d+.\d+.*)"`)
-	s := re.ReplaceAllString(buildFile, fmt.Sprintf(`${1}%s"`, GradlePluginVersion))
+	s := re.ReplaceAllString(buildFile, fmt.Sprintf(`${1}%s"`, version))
 
 	err = os.WriteFile(path, []byte(s), 0x644)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("updated %s to %s\n", path, GradlePluginVersion)
-
+	fmt.Printf("updated %s to %s\n", path, version)
 }
