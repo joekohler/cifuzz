joekohler/cifuzz

commit fd5c09efaa1724ea8c78cafb3bb63fd74ec2dc3a

fix(run): Print built output to stderr when --json is used

internal/cmd/run/run.go
@@ -293,7 +293,13 @@ depends on the build system configured for the project.
 
 			opts.argsToPass = argsToPass
 
-			opts.buildStdout = cmd.OutOrStdout()
+			if opts.PrintJSON {
+				// We only want JSON output on stdout, so we print the build
+				// output to stderr.
+				opts.buildStdout = cmd.ErrOrStderr()
+			} else {
+				opts.buildStdout = cmd.OutOrStdout()
+			}
 			opts.buildStderr = cmd.OutOrStderr()
 			if logging.ShouldLogBuildToFile() {
 				opts.buildStdout, err = logging.BuildOutputToFile(opts.ProjectDir, []string{opts.fuzzTest})
@@ -448,11 +454,7 @@ func (c *runCmd) run() error {
 func (c *runCmd) buildFuzzTest() (*build.Result, error) {
 	var err error
 
-	buildPrinterOutput := os.Stdout
-	if c.opts.PrintJSON {
-		buildPrinterOutput = os.Stderr
-	}
-	buildPrinter := logging.NewBuildPrinter(buildPrinterOutput, log.BuildInProgressMsg)
+	buildPrinter := logging.NewBuildPrinter(c.opts.buildStdout, log.BuildInProgressMsg)
 	defer func(err *error) {
 		if *err != nil {
 			buildPrinter.StopOnError(log.BuildInProgressErrorMsg)

commit 8c79da6d14655b47cb2435dd8577e2310c702789

fix: Fix build printer logging to build log file
Fixes an issue introduced by fd5c09efaa1724ea8c78cafb3bb63fd74ec2dc3a.

internal/cmd/run/run.go
@@ -454,7 +454,18 @@ func (c *runCmd) run() error {
 func (c *runCmd) buildFuzzTest() (*build.BuildResult, error) {
 	var err error
 
-	buildPrinter := logging.NewBuildPrinter(c.opts.buildStdout, log.BuildInProgressMsg)
+	// Note that the build printer should *not* print to c.opts.buildStdout,
+	// because that could be a file which is used to store the build log.
+	// We don't want the messages of the build printer to be printed to
+	// the build log file, so we let it print to stdout or stderr instead.
+	var buildPrinterOutput io.Writer
+	if c.opts.PrintJSON {
+		buildPrinterOutput = c.Command.OutOrStdout()
+	} else {
+		buildPrinterOutput = c.Command.ErrOrStderr()
+	}
+	buildPrinter := logging.NewBuildPrinter(buildPrinterOutput, log.BuildInProgressMsg)
+
 	defer func(err *error) {
 		if *err != nil {
 			buildPrinter.StopOnError(log.BuildInProgressErrorMsg)