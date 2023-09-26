joekohler/cifuzz

commit b6de650bc5fee7c37b527e0fbd44d56b07e18aca

fix(cmake): link dumper.o instead of compiling dumper from source

tools/cmake/modules/cifuzz-functions.cmake
@@ -259,15 +259,7 @@ function(add_fuzz_test name)
           target_link_options("${name}" PRIVATE -Wl,--wrap=__sanitizer_set_death_callback)
         endif()
       endif()
-      if(C IN_LIST _enabled_languages)
-        set(_dumper_src "${CIFUZZ_DUMPER_C_SRC}")
-      else()
-        if (NOT CXX IN_LIST _enabled_languages)
-          message(FATAL "CIFuzz: At least one of C and CXX has to be an enabled language")
-        endif()
-        set(_dumper_src "${CIFUZZ_DUMPER_CXX_SRC}")
-      endif()
-      target_sources("${name}" PRIVATE "${_dumper_src}")
+      target_link_libraries("${name}" PRIVATE "${CIFUZZ_DUMPER}")
     endif()
   else()
     message(FATAL_ERROR "cifuzz: Unsupported value for CIFUZZ_ENGINE: ${CIFUZZ_ENGINE}")

tools/cmake/cmake_test.go
@@ -46,6 +46,11 @@ func TestMain(m *testing.M) {
 		builder.Cleanup()
 		log.Fatalf("Failed to install CMake integration: %+v", err)
 	}
+	err = builder.BuildDumper()
+	if err != nil {
+		builder.Cleanup()
+		log.Fatalf("Failed to install CMake integration: %+v", err)
+	}
 	err = builder.CopyFiles()
 	if err != nil {
 		builder.Cleanup()

tools/cmake/modules/cifuzz-config.cmake
@@ -12,8 +12,8 @@ else()
 endif()
 set(CIFUZZ_INCLUDE_DIR "${CIFUZZ_CMAKE_DIR}/../../include" CACHE INTERNAL "The include directory for the cifuzz headers")
 set(CIFUZZ_DUMPER_C_SRC "${CIFUZZ_CMAKE_DIR}/../../src/dumper.c" CACHE INTERNAL "The path of the dumper as a C source file.")
-set(CIFUZZ_DUMPER_CXX_SRC "${CIFUZZ_CMAKE_DIR}/../../src/dumper.cpp" CACHE INTERNAL "The path of the dumper as a CXX source file.")
 set(CIFUZZ_LAUNCHER_C_SRC "${CIFUZZ_CMAKE_DIR}/../../src/launcher.c" CACHE INTERNAL "The path of the launcher as a C source file.")
 set(CIFUZZ_LAUNCHER_CXX_SRC "${CIFUZZ_CMAKE_DIR}/../../src/launcher.cpp" CACHE INTERNAL "The path of the launcher as a CXX source file.")
 set(CIFUZZ_REPLAYER_C_SRC "${CIFUZZ_CMAKE_DIR}/../../src/replayer.c" CACHE INTERNAL "The path of the replayer as a C source file.")
 set(CIFUZZ_REPLAYER_CXX_SRC "${CIFUZZ_CMAKE_DIR}/../../src/replayer.cpp" CACHE INTERNAL "The path of the replayer as a CXX source file.")
+set(CIFUZZ_DUMPER "${CIFUZZ_CMAKE_DIR}/../../lib/dumper.o" CACHE INTERNAL "The path of the dumper as an object file.")

commit 20e8546c9ceec1cb9964e1137d1944d7aa54b666

fix(cmake): Fix build with plain signature of target_link_libraries
Commit b6de650bc5fee7c37b527e0fbd44d56b07e18aca broke building fuzz
tests if the plain signature of target_link_libraries was used instead
of the keyword signature, for example:

    CMake Error at CMakeLists.txt:350 (target_link_libraries):
      The keyword signature for target_link_libraries has already been used with
      the target "fuzz_parse".  All uses of target_link_libraries with a target
      must be either all-keyword or all-plain.

      The uses of the keyword signature are here:

       * /home/adrian/.local/share/cifuzz/share/cmake/cifuzz-functions.cmake:262 (target_link_libraries)

This commit uses target_sources instead to support both
target_link_libraries with and without keywords.

Closes CLI-1304

tools/cmake/modules/cifuzz-functions.cmake
@@ -9,6 +9,11 @@ function(enable_fuzz_testing)
   # metadata for renamed or removed targets doesn't linger around.
   file(REMOVE_RECURSE "${CMAKE_BINARY_DIR}/$<CONFIG>/.cifuzz")
 
+  # Mark the dumper.o object file as a generated object file
+  set_source_files_properties("${CIFUZZ_DUMPER}" PROPERTIES
+                              GENERATED TRUE
+                              EXTERNAL_OBJECT TRUE)
+
   # Conceptually, "building for fuzzing" is similar to a build type such as Release or RelWithDebInfo. We instead use
   # a cache variable that adds flags to a base configuration we assume to be RelWithDebInfo for multiple reasons:
   # 1. Custom build types require defining a potentially unknown set of cache variables and are thus hard to maintain.
@@ -259,7 +264,7 @@ function(add_fuzz_test name)
           target_link_options("${name}" PRIVATE -Wl,--wrap=__sanitizer_set_death_callback)
         endif()
       endif()
-      target_link_libraries("${name}" PRIVATE "${CIFUZZ_DUMPER}")
+      target_sources("${name}" PRIVATE "${CIFUZZ_DUMPER}")
     endif()
   else()
     message(FATAL_ERROR "cifuzz: Unsupported value for CIFUZZ_ENGINE: ${CIFUZZ_ENGINE}")