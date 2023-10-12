dcb5bbbb9fcbe0c648e4f09c64d806065ac03ab3
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2023-10-12 11:19:11 +0200

    chore: update gradle plugin to 1.6.0

--- examples/gradle-kotlin/build.gradle.kts
+++ examples/gradle-kotlin/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
     // Include cifuzz
-    id("com.code-intelligence.cifuzz") version "1.5.0"
+    id("com.code-intelligence.cifuzz") version "1.6.0"
 }
 
 repositories {
--- examples/gradle-multi/testsuite/build.gradle.kts
+++ examples/gradle-multi/testsuite/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("com.code-intelligence.cifuzz") version "1.5.0"
+    id("com.code-intelligence.cifuzz") version "1.6.0"
 }
 
 repositories.mavenCentral()
