0e99dc72b50b7b53d88c261f97e192b0ca1dc9bf
Florian Gläßer <florian.glaesser@code-intelligence.com>
2023-10-30 14:15:20 +0100

    feat(options): add jazzerjs reporters flag

--- pkg/options/node.go
+++ pkg/options/node.go
@@ -6,6 +6,7 @@ import (
 
 const JazzerJSTestNamePattern string = "--testNamePattern"
 const JazzerJSTestPathPattern string = "--testPathPattern"
+const JazzerJSReporters string = "--reporters"
 const JazzerJSCoverageDirectory string = "--coverageDirectory"
 const JazzerJSCoverageReporters string = "--coverageReporters"
 const JestTestFailureExitCode string = "--testFailureExitCode"
@@ -16,6 +17,13 @@ func JazzerJSTestNamePatternFlag(value string) string {
 
 func JazzerJSTestPathPatternFlag(value string) string {
 	return JazzerJSTestPathPattern + fmt.Sprintf("='%s'", value)
+}
+
+func JazzerJSReportersFlag(value string) string {
+	if value == "" {
+		return JazzerJSReporters
+	}
+	return JazzerJSReporters + fmt.Sprintf("='%s'", value)
 }
 
 func JazzerJSCoverageDirectoryFlag(value string) string {
